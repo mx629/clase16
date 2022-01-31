cce17917eab0edbba2beb045b1dd0707949882a8
Max <mpfallik@gmail.com>
2022-01-31 13:07:53 -0300

    minor padding tweaks and nav hover effect

--- scripts.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    $(window).scroll(function () {
-        var currentScrollTop = $(window).scrollTop();
-        $('#hero-overlay').css('opacity',currentScrollTop/$('#hero-overlay').height());
-
-    });
-});
